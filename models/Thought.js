77ed06bf62f1e569c868740cb5a3bd8e940ab445
samlarson94 <samlarson94@gmail.com>
2022-05-16 21:42:03 -0500

    Building out models, Thought and User

---
+++ models/Thought.js
@@ -0,0 +1,68 @@
+const { Schema, model } = require("mongoose");
+const { moveMessagePortToContext } = require("worker_threads");
+const moment = require('moment');
+
+// Reaction Schema - Subdocument of Thought Schema
+const reactionSchema = new Schema(
+{
+    reactionId: {
+        type: mongoose.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moveMessagePortToContext(createdAtVal).format('MM DD, YYYY [a] hh:mm a'),
+    }
+
+});
+
+// Thought Schema 
+const ThoughtSchema = new Schema(
+{
+    thoughtText: {
+        type: String,
+        required: true, 
+        minLength: 1,
+        maxLength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moveMessagePortToContext(createdAtVal).format('MM DD, YYYY [a] hh:mm a'),
+        
+    },
+    username: 
+    {
+        type: String,
+        required: true
+       },
+    reactions: [
+        reactionSchema
+    ]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false,
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
