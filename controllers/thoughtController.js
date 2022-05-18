9c47d17cb16c59a74e45b24fd1a2e24a07a41636
samlarson94 <samlarson94@gmail.com>
2022-05-18 10:38:36 -0500

    Adding remaining route logic to thoughtController.js

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -28,6 +28,12 @@ addThought({ params, body }, res) {
     console.log(params);
     Thought.create(body)
         .then(dbThoughtData => {
+            // Add thought to thoughts array in User Model using findOneAndUpdate()
+            User.findOneAndUpdate(
+                { _id: params.userId },
+                { $push: { thoughts: dbThoughtData._id } },
+                { new: true, runValidators: true }
+            )
             console.log(dbThoughtData);
             res.json(dbThoughtData);
         })
@@ -35,11 +41,63 @@ addThought({ params, body }, res) {
 },
 
 //UPDATE THOUGHT
+updateThought({ params, body }, res) {
+    Thought.findOneAndUpdate(
+        { _id: params.userId },
+        { $set: body },
+        { new: true, runValidators: true }
+    )
+
+    .then(dbThoughtData => {
+        if (!dbThoughtData) {
+            res.status(404).json({ message: "Sorry, we were daydreaming. Could not find this thought." });
+            return;
+        } res.json(dbThoughtData);
+    })
+    .catch(err => res.json(err));
+},
 
 //ADD REACTION TO THOUGHT
+addReaction({ params, body }, res) {
+    console.log(params);
+    // Update through reaction array within Thought model
+    Thought.findOneAndUpdate(
+        {_id: params.thoughtId },
+        { $push: {reactions: body}},
+        { new: true, runValidators: true}
+    )
 
-//DELETE THOUGHT
+    .then(dbThoughtData => {
+        if (!dbThoughtData) {
+            res.status(404).json({message: 'Could not find a thought to react to.'});
+            return;
+        }
+        res.json(db.ThoughtData);
+    })
+    .catch(err => res.json(err));
+},
+//DELETE THOUGHT - And associated responses.
+removeThought({ params }, res) {
+    Thought.findOneAndDelete({ _id: params.thoughtId })
+        .then(deletedThought => {
+            if (!deletedThought) {
+                return res.status(404).json({ message: 'Could not find this thought.' });
+            }
+           
+        })
+        .catch(err => res.json(err));
+    },
 
 //DELETE REACTION TO THOUGHT
+deleteReaction({ params }, res) {
+    Thought.findOneAndUpdate(
+        { _id: params.thoughtId },
+        { $pull: { reactions: { reactionId: params.reactionId } } },
+        { new: true }
+    )
+    .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => res.json(err));
+    }
+};
 
 modules.exports = thoughtController;
